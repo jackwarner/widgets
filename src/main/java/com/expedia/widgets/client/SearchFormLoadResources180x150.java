a5cb6fb1f46cada96c2de06b88780c8b98e7f35b
Jack Warner <jackwarner@wmalumni.com>
2011-09-18 13:48:19 -0700

    placeholder class for splitpoints

---
+++ src/main/java/com/expedia/widgets/client/SearchFormLoadResources180x150.java
@@ -0,0 +1,14 @@
+package com.expedia.widgets.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface SearchFormLoadResources180x150 extends ClientBundle {
+	final String FOLDER = "searchform/";
+	final String SUBFOLDER_180x150 = "180x150/";
+
+	
+	@Source(FOLDER + SUBFOLDER_180x150 + "canvas180x150b.png")
+	ImageResource canvas180x150();
+
+}
